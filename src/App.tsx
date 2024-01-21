b57c82fa89a9ca9d0c5d6f5f81ee77084e3cefbd
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-01-21 21:47:30 +0900

    delete comments

--- src/App.tsx
+++ src/App.tsx
@@ -16,42 +16,6 @@ function App() {
       <main>
         <section className="flex gap-8 m-8 mt-0">
           <MotherTask title="Scheduled" todos={todos} />
-          {/* <article className="border min-w-96 p-4 flex flex-col gap-4">
-            <h2 className="text-2xl font-semibold">Scheduled</h2>
-            <div className="flex flex-col gap-2">
-              <div className="p-3 bg-gray-100">
-                <p>할 일을 만들었다</p>
-              </div>
-              <div className="p-3 bg-gray-100">
-                <p>할 일을 만들었다</p>
-              </div>
-            </div>
-          </article>
-          <article className="border min-w-96 p-4 flex flex-col gap-4">
-            <h2 className="text-2xl font-semibold">In Progress</h2>
-            <div className="flex flex-col gap-2">
-              <div className="p-3 bg-gray-100">
-                <p>할 일을 만들었다</p>
-              </div>
-              <div className="p-3 bg-gray-100">
-                <p>할 일을 만들었다</p>
-              </div>
-            </div>
-          </article>
-          <article className="border min-w-96 p-4 flex flex-col gap-4">
-            <h2 className="text-2xl font-semibold">Hold</h2>
-            <div className="flex flex-col gap-2">
-              <div className="p-3 bg-gray-100">
-                <p>할 일을 만들었다</p>
-              </div>
-              <div className="p-3 bg-gray-100">
-                <p>할 일을 만들었다</p>
-              </div>
-            </div>
-          </article> */}
-        </section>
-        <section className="fixed bottom-0 w-full bg-gray-100 p-6 flex justify-center items-center">
-          <input type="text" className="w-full h-14 border-2" />
         </section>
       </main>
     </>

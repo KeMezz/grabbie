fafdda33f6fb2a9791bab53c60e758248d8d6cf9
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-01-25 23:41:24 +0900

    attach with react-hook-form

--- src/components/AddNewTask.tsx
+++ src/components/AddNewTask.tsx
@@ -1,79 +1,91 @@
 import React from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import MotherSelector from "./MotherSelector";
+import { useForm } from "react-hook-form";
+import { AddNewTaskFormData } from "../types/add-new-task.type";
 
 const AddNewTask: React.FC = () => {
+  const { register, handleSubmit, control } = useForm<AddNewTaskFormData>();
+  const onSubmit = (formData: AddNewTaskFormData) => {
+    console.log(formData);
+  };
   return (
-    <Dialog.Root>
-      <Dialog.Trigger asChild>
-        <button className="flex items-center gap-2 text-zinc-400 hover:text-zinc-900">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Dialog.Root>
+        <Dialog.Trigger asChild>
+          <button
+            className="flex items-center gap-2 text-zinc-400 hover:text-zinc-900"
+            type="button"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-            />
-          </svg>
-          <p>Add New Task</p>
-        </button>
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-          <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
-            새로운 할 일 만들기
-          </Dialog.Title>
-          <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
-            어떤 할 일을 만들고 싶으신가요?
-          </Dialog.Description>
-          <form>
-            <MotherSelector />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              />
+            </svg>
+            <p>Add New Task</p>
+          </button>
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-blackA6 data-[state=open]:animate-overlayShow fixed inset-0" />
+          <Dialog.Content className="data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
+            <Dialog.Title className="text-mauve12 m-0 text-[17px] font-medium">
+              새로운 할 일 만들기
+            </Dialog.Title>
+            <Dialog.Description className="text-mauve11 mt-[10px] mb-5 text-[15px] leading-normal">
+              어떤 할 일을 만들고 싶으신가요?
+            </Dialog.Description>
+            <MotherSelector control={control} />
             <textarea
-              className="border rounded-lg w-full p-4 outline-zinc-700"
-              name="text"
-              id="text"
               rows={5}
+              className="border rounded-lg w-full p-4 outline-zinc-700"
               placeholder="할 일을 입력해주세요."
+              {...register("text", { required: true })}
             ></textarea>
-          </form>
-          <div className="mt-[25px] flex justify-end">
+            <div className="mt-[25px] flex justify-end">
+              <Dialog.Close asChild>
+                <button
+                  className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none"
+                  type="submit"
+                >
+                  추가하기
+                </button>
+              </Dialog.Close>
+            </div>
             <Dialog.Close asChild>
-              <button className="bg-green4 text-green11 hover:bg-green5 focus:shadow-green7 inline-flex h-[35px] items-center justify-center rounded-[4px] px-[15px] font-medium leading-none focus:shadow-[0_0_0_2px] focus:outline-none">
-                추가하기
+              <button
+                className="text-zinc-700 hover:bg-zinc-100 focus:shadow-zinc-200 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
+                aria-label="Close"
+                type="button"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18 18 6M6 6l12 12"
+                  />
+                </svg>
               </button>
             </Dialog.Close>
-          </div>
-          <Dialog.Close asChild>
-            <button
-              className="text-zinc-700 hover:bg-zinc-100 focus:shadow-zinc-200 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:shadow-[0_0_0_2px] focus:outline-none"
-              aria-label="Close"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6 18 18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </form>
   );
 };
 
--- src/components/MotherSelector.tsx
+++ src/components/MotherSelector.tsx
@@ -1,75 +1,87 @@
 import * as Select from "@radix-ui/react-select";
 import React from "react";
 import SelectItem from "./SelectItem";
+import { Control, Controller } from "react-hook-form";
+import { AddNewTaskFormData } from "../types/add-new-task.type";
 
-const MotherSelector: React.FC = () => {
+interface MotherSelectorProps {
+  control: Control<AddNewTaskFormData>;
+}
+
+const MotherSelector: React.FC<MotherSelectorProps> = ({ control }) => {
   return (
-    <Select.Root>
-      <Select.Trigger
-        className="inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-zinc-700 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-zinc-900 outline-none mb-4 w-full"
-        aria-label="Sections"
-      >
-        <Select.Value placeholder="어느 섹션에 추가할까요?" />
-        <Select.Icon className="text-zinc-700">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-5 h-5"
+    <Controller
+      name="section"
+      control={control}
+      render={() => (
+        <Select.Root>
+          <Select.Trigger
+            className="inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-zinc-700 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-zinc-900 outline-none mb-4 w-full"
+            aria-label="Sections"
           >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="m19.5 8.25-7.5 7.5-7.5-7.5"
-            />
-          </svg>
-        </Select.Icon>
-      </Select.Trigger>
-      <Select.Portal>
-        <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-          <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-zinc-700 cursor-default">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m4.5 15.75 7.5-7.5 7.5 7.5"
-              />
-            </svg>
-          </Select.ScrollUpButton>
-          <Select.Viewport className="p-[5px]">
-            <SelectItem value="scheduled">Scheduled</SelectItem>
-            <SelectItem value="doing">Doing</SelectItem>
-            <SelectItem value="done">Done</SelectItem>
-            <SelectItem value="hold">Hold</SelectItem>
-          </Select.Viewport>
-          <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-zinc-700 cursor-default">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="m19.5 8.25-7.5 7.5-7.5-7.5"
-              />
-            </svg>
-          </Select.ScrollDownButton>
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
+            <Select.Value placeholder="어느 섹션에 추가할까요?" />
+            <Select.Icon className="text-zinc-700">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5 h-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                />
+              </svg>
+            </Select.Icon>
+          </Select.Trigger>
+          <Select.Portal>
+            <Select.Content className="overflow-hidden bg-white rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
+              <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-zinc-700 cursor-default">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m4.5 15.75 7.5-7.5 7.5 7.5"
+                  />
+                </svg>
+              </Select.ScrollUpButton>
+              <Select.Viewport className="p-[5px]">
+                <SelectItem value="scheduled">Scheduled</SelectItem>
+                <SelectItem value="doing">Doing</SelectItem>
+                <SelectItem value="done">Done</SelectItem>
+                <SelectItem value="hold">Hold</SelectItem>
+              </Select.Viewport>
+              <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-zinc-700 cursor-default">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-5 h-5"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="m19.5 8.25-7.5 7.5-7.5-7.5"
+                  />
+                </svg>
+              </Select.ScrollDownButton>
+            </Select.Content>
+          </Select.Portal>
+        </Select.Root>
+      )}
+    ></Controller>
   );
 };
 

c3a8537a0074c8edbef746e9d15c44c30635c899
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-01-26 00:11:47 +0900

    connect onchange event w/field

--- src/components/MotherSelector.tsx
+++ src/components/MotherSelector.tsx
@@ -13,8 +13,9 @@ const MotherSelector: React.FC<MotherSelectorProps> = ({ control }) => {
     <Controller
       name="section"
       control={control}
-      render={() => (
-        <Select.Root>
+      rules={{ required: true }}
+      render={({ field: { onChange } }) => (
+        <Select.Root onValueChange={onChange}>
           <Select.Trigger
             className="inline-flex items-center justify-between rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-zinc-700 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-zinc-900 outline-none mb-4 w-full"
             aria-label="Sections"

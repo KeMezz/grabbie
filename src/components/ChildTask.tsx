5e11e2a6217a25696bd9e82d93b05dbfedc6ecb3
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-01-27 22:09:14 +0900

    add created date of todo

--- src/components/ChildTask.tsx
+++ src/components/ChildTask.tsx
@@ -5,14 +5,15 @@ interface ChildTaskProps {
   id: number;
   text: string;
   isDone: boolean;
+  createdAt: number;
 }
 
-const ChildTask: React.FC<ChildTaskProps> = ({ id, text }) => {
+const ChildTask: React.FC<ChildTaskProps> = ({ createdAt, text }) => {
   return (
     <div className="p-3 rounded-lg bg-white shadow-sm flex flex-col gap-3">
       <p>{text}</p>
       <span className="text-xs text-gray-400 ml-auto">
-        {dayjs(id).fromNow()}
+        {dayjs(createdAt).fromNow()}
       </span>
     </div>
   );

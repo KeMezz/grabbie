84dcf63085afeb85eb875b1ac3dec64a0db71648
정형진 <88423849+KeMezz@users.noreply.github.com>
2024-01-28 23:27:42 +0900

    disable react strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
@@ -6,9 +6,9 @@ import "./dayjs.ts";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
-  </React.StrictMode>
+  //  <React.StrictMode>
+  <RecoilRoot>
+    <App />
+  </RecoilRoot>
+  //  </React.StrictMode>
 );
